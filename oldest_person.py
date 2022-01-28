9b285bfb0587c73ccdfc5c9f232ccea24f460976
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-28 11:12:02 +0500

    Task 06

--- oldest_person.py
+++ oldest_person.py
@@ -1,3 +1,4 @@
+import math
 def oldest(people:dict):
     """
     Given a dictionary containing the names and ages of a group of people, return the name of the oldest person.
@@ -6,4 +7,14 @@ def oldest(people:dict):
     Returns:
         str: the name of the oldest person
     """
-    return
+    # k=[]
+    # s=people.values()
+    # for x in s:
+    #     k.append(x)
+    # l=max(k)
+    # if l in people.values():
+    #     return people.values(l)    
+    return max(people)
+    
+        
+print(oldest({"Javohir": 22, "Sharof": 23, "Tolib": 34, "Rustam": 16}))
